7e09cd162ee5b7c1b7d84b28e1d00a0bdcf24557
Hendrik Busch <h.m.busch@googlemail.com>
2013-06-16 14:21:57 +0200

    Defined battery monitoring for Nanoquad frame
    The nanoquad is already wired up for battery voltage monitoring. Now monitoring has been activated in the software and calibrated accordingly (2S setup).

--- MultiWii_shared/config.h
+++ MultiWii_shared/config.h
@@ -817,12 +817,12 @@
        with R1=33k and R2=51k
        vbat = [0;1023]*16/VBATSCALE
        must be associated with #define BUZZER ! */
-    //#define VBAT              // uncomment this line to activate the vbat code
-    #define VBATSCALE       131 // (*) change this value if readed Battery voltage is different than real voltage
-    #define VBATNOMINAL     126 // 12,6V full battery nominal voltage - only used for lcd.telemetry
-    #define VBATLEVEL_WARN1 107 // (*) 10,7V
-    #define VBATLEVEL_WARN2  99 // (*) 9.9V
-    #define VBATLEVEL_CRIT   93 // (*) 9.3V - critical condition: if vbat ever goes below this value, permanent alarm is triggered
+    #define VBAT                 // uncomment this line to activate the vbat code
+    #define VBATSCALE       210  // (*) change this value if readed Battery voltage is different than real voltage
+    #define VBATNOMINAL      84  // 8.4V full battery nominal voltage - only used for lcd.telemetry
+    #define VBATLEVEL_WARN1  68  // (*) 6.8V
+    #define VBATLEVEL_WARN2  65  // (*) 6.5V
+    #define VBATLEVEL_CRIT   61  // (*) 6.1V - critical condition: if vbat ever goes below this value, permanent alarm is triggered
     #define NO_VBAT          16  // (*) Avoid beeping without any battery
 
 
